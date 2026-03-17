5c9072a37904d98ec2a74c09c3317d7b420fda57
Mohamed Eid <162780854+Mohamed-Eid-ElSayed@users.noreply.github.com>
2026-03-17 09:07:16 +0200

    Add Footer component to Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import UpcomingMovies from '../Components/UpCommingMovies'
 import MoreContent from '../Components/MoreContent'
+import Footer from '../Components/Footer'
 function Home() {
   return (
     <div className="flex py-5 flex-col-reverse md:flex-row">
       <UpcomingMovies/>
       <MoreContent/>
+      <Footer/>
     </div>
   )
 }
